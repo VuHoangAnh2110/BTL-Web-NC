60c61f28c59b240dfdbf2c925c8fa1a0861223f5
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2025-03-24 19:03:15 +0700

    update login

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    // Hiển thị thông báo dạng toast ở góc phải trên cùng
+    function showToast(type, message) {
+        Swal.fire({
+            toast: true,
+            position: 'top-end', // Vị trí góc phải trên cùng
+            icon: type, // 'success', 'error', 'warning', 'info'
+            title: message,
+            showConfirmButton: false,
+            timer: 3000 // Tự động ẩn sau 3 giây
+        });
+    }
+
+    
+});
