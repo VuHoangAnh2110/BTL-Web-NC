c67f7d7f19d9b941de7ae45cd0dc5511fb3fb377
VuHoangAnh2110 <134081915+VuHoangAnh2110@users.noreply.github.com>
2025-04-02 18:14:28 +0700

    cập nhật quản lý tài khoản và khóa tài khoản

--- Controllers/QLyTaiKhoanController.cs
+++ Controllers/QLyTaiKhoanController.cs
@@ -15,13 +15,52 @@ namespace BTL_Web_NC.Controllers
             _taiKhoanRepo = taiKhoanRepo;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            // var accounts = await _taiKhoanRepo.GetAllAsync();
-            return View();
+            var danhSachTaiKhoan = await _taiKhoanRepo.GetAllAsync(); // Lấy tất cả tài khoản
+            return View(danhSachTaiKhoan);
         }
 
-      
+        [HttpPost]
+        public async Task<IActionResult> KhoaTaiKhoan(string ipKhoaid)
+        {
+            if (string.IsNullOrEmpty(ipKhoaid))
+            {
+                return Json(new { success = false, message = "Thiếu thông tin tài khoản" });
+            }
+
+            var account = await _taiKhoanRepo.GetByTenTaiKhoanAsync(ipKhoaid);
+            if (account == null)
+            {
+                return Json(new { success = false, message = "Không tìm thấy tài khoản" });
+            }
+
+            string message;
+
+            // Nếu trạng thái = 2 (Hoạt động) thì chuyển thành 3 (Bị khóa) và ngược lại
+            if (account.TrangThai == 2)
+            {
+                account.TrangThai = 3;
+                message = "Tài khoản đã bị khóa";
+            }
+            else if (account.TrangThai == 3)
+            {
+                account.TrangThai = 2;
+                message = "Tài khoản đã được mở khóa";
+            }
+            else
+            {
+                return Json(new { success = false, message = "Tài khoản không thể khóa" });
+            }
+            await _taiKhoanRepo.UpdateAsync(account);
+            HttpContext.Session.SetString("SuccessMessage", "Cập nhật trạng thái thành công!");
+
+            return Json(new
+            {
+                success = true,
+                message = message
+            });
+        }
 
         public async Task<IActionResult> Details(int id)
         {
@@ -33,15 +72,15 @@ namespace BTL_Web_NC.Controllers
             return View(account);
         }
 
-        public async Task<IActionResult> Edit(int id)
-        {
-            var account = await _taiKhoanRepo.GetByIdAsync(id);
-            if (account == null)
-            {
-                return NotFound();
-            }
-            return View(account);
-        }
+        // public async Task<IActionResult> Edit(int id)
+        // {
+        //     var account = await _taiKhoanRepo.GetByIdAsync(id);
+        //     if (account == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return View(account);
+        // }
 
         // [HttpPost]
     //     public async Task<IActionResult> Edit(AccountViewModel model)
